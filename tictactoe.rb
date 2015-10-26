a0d38f7296e0541e404d246b399d7d1923b04221
Blake Strickland <Stricklandb02@gmail.com>
2015-10-26 16:30:59 -0400

    Alternating names displayed

--- tictactoe.rb
+++ tictactoe.rb
@@ -3,24 +3,22 @@ require './position'
 require './board'
 
 puts "Let's play!"
-# puts "Player X, enter your name!"
-# xname = gets.chomp
-# puts "Player O, enter your name!"
-# oname = gets.chomp
+puts "Player X, enter your name!"
+xname = gets.chomp
+puts "Player O, enter your name!"
+oname = gets.chomp
 
 new_board = Board.new
-
 new_board.display_board
-
+name = xname
 while new_board.board.any? {|x| x.include?(" ")}
 
-  puts "Enter your move: "
+  puts "#{name} your move: "
   move = gets.chomp
   if move.empty? || move.length != 2
     puts "Invalid move."
     redo
   end
-
 
   position = Position.new
   if position.validate_move?(move, new_board)
@@ -30,6 +28,6 @@ while new_board.board.any? {|x| x.include?(" ")}
   else
     puts "Invalid move2"
   end
-
+  name == xname ? (name = oname) : (name = xname)
 end
 puts "Game Over"
